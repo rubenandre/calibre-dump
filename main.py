4bef13a958b9d9cf649920fdd8557bf1017b0782
Rúben Silva <rubensilva945@gmail.com>
2020-04-03 20:24:47 +0100

    fix(calibredump): fixed #1 #2

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ SHODAN_API_KEY = 'SHODAN_KEY_HERE'
 class CalibreDump:
     def __init__(self, shodan_key):
         self.api = shodan.Shodan(shodan_key)
-        self.hosts = set()
-        self.dorks = ['/interface-data/init', '/browse/categories/allbooks']
+        self.hosts = []
 
     @staticmethod
-    def check_open(self, ip_port):
-        for dork in self.dorks:
+    def check_open(ip_port):
+        dorks = ['/interface-data/init', '/browse/categories/allbooks']
+        for dork in dorks:
             request = requests.get(f'http://{ip_port}{dork}')
             if request.status_code == 200:
                 return True
@@ -22,13 +22,12 @@ class CalibreDump:
             results = self.api.search('calibre')
 
             for result in results['matches']:
-                self.hosts.add(f"{result['ip_str']}:{result['port']}")
+                self.hosts.append(f"{result['ip_str']}:{result['port']}")
 
         except shodan.APIError as e:
             print(f'Error {e}')
 
 
 if __name__ == '__main__':
-    api = shodan.Shodan(SHODAN_API_KEY)
-    hosts = []
+    calibre = CalibreDump(SHODAN_API_KEY)
 
